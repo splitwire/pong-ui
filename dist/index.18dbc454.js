dc031188992ec3b265d5aabc5e7b1954d4465975
Clifford Kasper <clifford.kasper@gmail.com>
2023-02-15 12:08:18 -0600

    initial commit

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -594,7 +594,60 @@ class Game {
 }
 const game = new Game("pong");
 
-},{"19705b4963c7095b":"ep9Uy","d8beaedf91f89f23":"2k4ca","ac3c1562c6996374":"hMVTW"}],"ep9Uy":[function(require,module,exports) {
+},{"ac3c1562c6996374":"hMVTW","19705b4963c7095b":"ep9Uy","d8beaedf91f89f23":"2k4ca"}],"hMVTW":[function(require,module,exports) {
+class Ball {
+    static #ballRadius = 20;
+    static #ballColor = "red";
+    #canvas;
+    #ctx;
+    #position = {
+        x: 0,
+        y: 0
+    };
+    #direction = {
+        x: 0,
+        y: 0
+    };
+    #speed = 4;
+    constructor(canvas){
+        this.#canvas = canvas;
+        this.#ctx = canvas.getContext("2d");
+        this.initialize();
+    }
+    initialize() {
+        this.#position.x = this.#canvas.width / 2;
+        this.#position.y = this.#canvas.height / 2;
+        this.#direction.x = Math.round(Math.random()) === 1 ? 1 : -1;
+        this.#direction.y = Math.round(Math.random()) === 1 ? 1 : -1;
+    }
+    move() {
+        const radius = Ball.#ballRadius;
+        const canvas = this.#canvas;
+        const pos = this.#position;
+        const bounds = {
+            left: pos.x - radius,
+            right: pos.x + radius,
+            top: pos.y - radius,
+            bottom: pos.y + radius
+        };
+        if (bounds.left <= 0 || bounds.right > canvas.width) this.#direction.x *= -1;
+        if (bounds.top < 0 || bounds.bottom > canvas.height) this.#direction.y *= -1;
+        pos.x += this.#direction.x * this.#speed;
+        pos.y += this.#direction.y * this.#speed;
+    }
+    draw() {
+        const ctx = this.#ctx;
+        const pos = this.#position;
+        this.move();
+        ctx.beginPath();
+        ctx.arc(pos.x, pos.y, Ball.#ballRadius, 0, 2 * Math.PI);
+        ctx.fillStyle = Ball.#ballColor;
+        ctx.fill();
+    }
+}
+module.exports = Ball;
+
+},{}],"ep9Uy":[function(require,module,exports) {
 class Field {
     static #markerColor = "#FFC882";
     #canvas;
@@ -694,59 +747,6 @@ class Player {
     }
 }
 module.exports = Player;
-
-},{}],"hMVTW":[function(require,module,exports) {
-class Ball {
-    static #ballRadius = 20;
-    static #ballColor = "red";
-    #canvas;
-    #ctx;
-    #position = {
-        x: 0,
-        y: 0
-    };
-    #direction = {
-        x: 0,
-        y: 0
-    };
-    #speed = 4;
-    constructor(canvas){
-        this.#canvas = canvas;
-        this.#ctx = canvas.getContext("2d");
-        this.initialize();
-    }
-    initialize() {
-        this.#position.x = this.#canvas.width / 2;
-        this.#position.y = this.#canvas.height / 2;
-        this.#direction.x = Math.round(Math.random()) === 1 ? 1 : -1;
-        this.#direction.y = Math.round(Math.random()) === 1 ? 1 : -1;
-    }
-    move() {
-        const radius = Ball.#ballRadius;
-        const canvas = this.#canvas;
-        const pos = this.#position;
-        const bounds = {
-            left: pos.x - radius,
-            right: pos.x + radius,
-            top: pos.y - radius,
-            bottom: pos.y + radius
-        };
-        if (bounds.left <= 0 || bounds.right > canvas.width) this.#direction.x *= -1;
-        if (bounds.top < 0 || bounds.bottom > canvas.height) this.#direction.y *= -1;
-        pos.x += this.#direction.x * this.#speed;
-        pos.y += this.#direction.y * this.#speed;
-    }
-    draw() {
-        const ctx = this.#ctx;
-        const pos = this.#position;
-        this.move();
-        ctx.beginPath();
-        ctx.arc(pos.x, pos.y, Ball.#ballRadius, 0, 2 * Math.PI);
-        ctx.fillStyle = Ball.#ballColor;
-        ctx.fill();
-    }
-}
-module.exports = Ball;
 
 },{}]},["9tRox","1SICI"], "1SICI", "parcelRequire1357")
 
